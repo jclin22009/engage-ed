33f992ef9c4479dd083ff4d34c9c57cc9bca00ea
Jaiden Reddy <jaidenreddy@gmail.com>
2022-02-19 21:22:59 -0800

    Integrate multiple choice and short answer

---
+++ src/shortAnswer/MultipleChoice.js
@@ -0,0 +1,24 @@
+// export default class MultipleChoice {
+//   constructor(id, question) {
+//     this.id = id;
+//     this.question = question;
+//     this.answers = [];
+//   }
+//
+//   addToDOM(parent) {
+//
+//   }
+//
+//   _createInput() {
+//     let form = document.createElement("form");
+//     form.classList.add("mcForm");
+//
+//     let input = document.createElement("input");
+//     input.type = "text";
+//     let label = document.createElement("label");
+//     label.for = input;
+//     input.innerHTML = "Add Option:";
+//     form.append(input);
+//     form.append(label);
+//   }
+// }
