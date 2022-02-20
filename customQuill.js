3536aacadc0dfcd48a3d7bb33e0003c9303e2ed5
yanniksingh <ynsingh0@gmail.com>
2022-02-19 17:02:42 -0800

    Add more editor controls and make landing page smoother

--- customQuill.js
+++ customQuill.js
@@ -1,7 +1,24 @@
 var editor = new Quill('#editor', {
-  modules: { toolbar: '#toolbar' },
+  modules: {
+    toolbar: [
+      [{ header: [1, 2, 3,  false] }],
+      ['bold', 'italic', 'underline','strike'],
+      ['image'],
+      ['link'],
+      [{ 'script': 'sub'}, { 'script': 'super' }],
+      [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+      ['clean']
+    ]
+  },
   theme: 'snow'
 });
+
+function getHtml() {
+  console.log(editor.root.innerHTML);
+  /*const fs = require('node_modules/fs').promises;*/
+  /*var textData = editor.root.innerHTML;
+  await fs.writeFile('myFile.txt', textData);*/
+}
 
 var Parchment = Quill.import("parchment");
 
@@ -13,11 +30,11 @@ Quill.register(CustomClass, true);
 
 var customButton = document.querySelector('#custom-button');
 customButton.addEventListener('click', function() {
-  var format = quill.getFormat();
+  var format = editor.getFormat();
   if(format.custom) {
-    quill.format('custom', '');
+    editor.format('custom', '');
   } else {
-    quill.format('custom', 'test');
+    editor.format('custom', 'test');
   }
   const cursorPosition = this.quill.getSelection().index;
   this.quill.insertText(cursorPosition, "amogus");
@@ -25,8 +42,8 @@ customButton.addEventListener('click', function() {
   console.log("hello from first function");
 });
 
-quill.on('editor-change', function(eventName, ...args) {
-  if(quill.getFormat().custom) {
+editor.on('editor-change', function(eventName, ...args) {
+  if(editor.getFormat().custom) {
     customButton.classList.add('ql-active');
   } else {
     customButton.classList.remove('ql-active');
